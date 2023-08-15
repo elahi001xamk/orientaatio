85f8d75b91137a56e5c047c3114eb2d81c427829
Laura Hietala <elahi001@edu.xamk.fi>
2023-08-15 19:55:48 +0300

    valmis

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Heippa maailma, Laura kävi täällä!
         </p>
         <a
           className="App-link"
